9013128800c6827bc149a8ec3b6378eb8065212c
Leonardo Ariel Scandura Garcia <leoarielgarcia87@gmail.com>
2022-12-08 12:52:18 +0100

    a

--- mail.php
+++ mail.php
@@ -1,86 +1,82 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ERROR);
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer/src/Exception.php';
-    require 'PHPMailer/src/PHPMailer.php';
-    require 'PHPMailer/src/SMTP.php';
-    
-$error = "";
 
-function mail_phpmailer_2018($destinatario_email, $destinatario_nombre, $remitente_email, $nombre, $contenido_asunto, $contenido_html){
+  // ini_set('display_errors', 1);
+  // ini_set('display_startup_errors', 1);
+  // error_reporting(E_ERROR);
 
-    global $error;
-    $paso ='LiceoRC4';
-    $host_smtp         = "smtp.gmail.com";
-    $login_smtp        = "leoarielgarcia87@gmail.com";
-    $password_smtp     = $paso;
+  require '/var/www/html/vendor/autoload.php';
+  use PHPMailer\PHPMailer\PHPMailer;
+  use PHPMailer\PHPMailer\SMTP;
+  use PHPMailer\PHPMailer\Exception;
+  $error = "";
 
-    $mail             = new PHPMailer();
+  function mail_phpmailer_2018($destinatario_email, $destinatario_nombre,$nombre, $remitente_email, $telefono, $contenido_asunto, $contenido_html) {
 
-    //  $mail->SMTPDebug = 2; 
-    $mail->isSMTP(); 
+      global $error;
+      $host_smtp         = "smtp.ionos.es";
+      $login_smtp        = "info@altposicionamientopremium.com";
+      $password_smtp     = 'J4DTKw*+75gcRZg';
+
+      $mail             = new PHPMailer();
+
+      // $mail->SMTPDebug = SMTP::DEBUG_SERVER; 
+      $mail->isSMTP(); 
+      $mail->Host       = $host_smtp;
+      $mail->SMTPAuth   = true;
+      $mail->Username   = $login_smtp; // SMTP account username
+      $mail->Password   = $password_smtp;        // SMTP account password
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+      $mail->Port       = 465;                    // set the SMTP port for the GMAIL server
+      $mail->Host       = $host_smtp; // sets the SMTP server
+
+      
+      
+      $mail->SetFrom($remitente_email, $nombre);
+      $mail->AddAddress($destinatario_email, $destinatario_nombre);
+      $mail->Subject = $contenido_asunto;
+      $mail->MsgHTML($contenido_html);
+
+      $mail->isHTML(true);
+
+      
+
+      if(!$mail->Send()) {
+        return false;
+      } else {
+        return true;
+      }
+  }
  
-    $mail->Host       = $host_smtp;
-    $mail->SMTPAuth   = true;
-    $mail->Username   = $login_smtp; // SMTP account username
-    $mail->Password   = $password_smtp;        // SMTP account password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port       = 465;                    // set the SMTP port for the GMAIL server
-    $mail->Host       = $host_smtp; // sets the SMTP server
-
-    $mail->SetFrom($remitente_email, $nombre);
-    $mail->AddAddress($destinatario_email, $destinatario_nombre);
-    $mail->Subject    = $contenido_asunto;
-    $mail->MsgHTML($contenido_html);
-
-    $mail->isHTML(true);
-
-    if(!$mail->Send()) {
-      return false;
-    } else {
-      return true;
-    }
-}
-
-$nombre   = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["nombre"])));
-$email    = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-
-$mensaje  = trim($_POST["mensaje"]);
-//$to ='ns.desarrollo16@gmail.com';
-$to = 'leoarielgarcia87@gmail.com';
-$enviado = false;
-$header = 'From:'. $_POST["email"] . "\r\n" .
+ // $destinatario_email_1 = "info@altposicionamientopremium.com";
+  $destinatario_email_1 = "leoarielgarcia87@gmail.com";
+  $destinatario_nombre = str_replace(array("\r","\n"), array(" "," ") , strip_tags(trim($_POST["nombre"])));
+  $remitente_email = $_POST["email"];
+  $remitente_nombre = "Leo Scandura";
+  $telefono = trim($_POST["telefono"]);
+  $contenido_asunto = "mensaje";
+  $nombre = trim($_POST["nombre"]);
+  $enviado = false;
+  $cuerpo = "";
+  $cuerpo .= "
+  <h4>Solicitud Informacion</h4><br/>
+  <ul>
+      <li>Nombre : ". $_POST["nombre"] . "</li>
+      <li>Email : ". $_POST["email"] . "</li>
+      <li>Telefono : ". $_POST["telefono"] . "</li>
+      <li>Comentarios : ". $_POST["mensaje"] . "</li> 
+  </ul>
+  ";
 
 
-$cuerpo = "";
-$cuerpo .= "
-<h4>Solicitud Informacion</h4><br/>
-<ul>
-    <li>Nombre : ". $_POST["nombre"] . "</li>
-    <li>Email : ". $_POST["email"] . "</li>
-    <li>Telefono : ". $_POST["telefono"] . "</li>
-    <li>Comentarios : ". $_POST["mensaje"] . "</li>
-";
+$contenido_asunto = $_POST["nombre"] . " ha enviado un mensaje en la web leoscandura.es" . $cuerpo;
+ 
 
+$enviado = mail_phpmailer_2018($destinatario_email_1, $destinatario_nombre,$nombre, $remitente_email, $telefono, $contenido_asunto, $cuerpo);
 
-
-
-$contenido_asunto = $cuerpo . "este mensaje fue enviado en la web";
-$destinatario_email     =    "leoarielgarcia87@gmail.com";
-$destinatario_nombre     =    "Leonardo Scandura";
-
-$enviado = mail_phpmailer_2018($destinatario_email, $destinatario_nombre, $remitente_email, $email, $nombre, $contenido_asunto, $cuerpo);
-echo $cuerpo;
-echo $enviado;
-echo ($enviado);
- if($enviado){
-    header("Location: enviado.php");
- } else {
-     echo 0;
- }
+  if($enviado){
+      echo "si";
+  } else {
+      echo "no";
+  }
 
 ?>
- 
