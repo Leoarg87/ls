8a20b7577c6cdc0c4f885503728f71265098232a
Leonardo Ariel Scandura Garcia <leoarielgarcia87@gmail.com>
2022-12-08 13:37:38 +0100

    a

--- mail.php
+++ mail.php
@@ -50,7 +50,7 @@ $email    = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
 
 $mensaje  = trim($_POST["mensaje"]);
 //$to ='ns.desarrollo16@gmail.com';
-$to = 'leoarielgarcia87@gmail.com';
+$to = 'hola@leoscandura.es';
 $enviado = false;
 $header = 'From:'. $_POST["email"] . "\r\n" .
 
@@ -69,7 +69,7 @@ $cuerpo .= "
 
 
 $contenido_asunto = $cuerpo . "este mensaje fue enviado en la web";
-$destinatario_email     =    "leoarielgarcia87@gmail.com";
+$destinatario_email     =    "hola@leoscandura.es";
 $destinatario_nombre     =    "Leonardo Scandura";
 
 $enviado = mail_phpmailer_2018($destinatario_email, $destinatario_nombre, $remitente_email, $email, $nombre, $contenido_asunto, $cuerpo);
@@ -77,7 +77,7 @@ echo $cuerpo;
 echo $enviado;
 echo ($enviado);
  if($enviado){
-    header("Location: enviado.php");
+  echo 1;
  } else {
      echo 0;
  }
