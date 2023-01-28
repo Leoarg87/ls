7ea1b6e7e891f24110c01a3594f99e88bd9fe31a
Leonardo Ariel Scandura Garcia <leoarielgarcia87@gmail.com>
2023-01-28 13:26:17 +0100

    a

--- rodo/mail.php
+++ rodo/mail.php
@@ -17,13 +17,12 @@ function mail_phpmailer_2018($destinatario_email, $destinatario_nombre, $remiten
     $host_smtp         = "smtp.hostinger.com";
     $login_smtp        = "hola@leoscandura.es";
     $password_smtp     = $paso;
-    $nombre   = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["nombre"])));
-    $remitente_email    = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+
     $mail             = new PHPMailer();
 
     //  $mail->SMTPDebug = 2; 
     $mail->isSMTP(); 
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER; 
+ 
     $mail->Host       = $host_smtp;
     $mail->SMTPAuth   = true;
     $mail->Username   = $login_smtp; // SMTP account username
@@ -31,11 +30,11 @@ function mail_phpmailer_2018($destinatario_email, $destinatario_nombre, $remiten
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = 465;                    // set the SMTP port for the GMAIL server
     $mail->Host       = $host_smtp; // sets the SMTP server
+
     $mail->SetFrom($remitente_email, $nombre);
     $mail->AddAddress($destinatario_email, $destinatario_nombre);
     $mail->Subject    = $contenido_asunto;
     $mail->MsgHTML($contenido_html);
-    
 
     $mail->isHTML(true);
 
@@ -46,7 +45,8 @@ function mail_phpmailer_2018($destinatario_email, $destinatario_nombre, $remiten
     }
 }
 
-
+$nombre   = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["nombre"])));
+$remitente_email    = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
 
 $mensaje  = trim($_POST["mensaje"]);
 //$to ='ns.desarrollo16@gmail.com';
@@ -73,7 +73,9 @@ $destinatario_email     =    "leoarielgarcia87@gmail.com";
 $destinatario_nombre     =    "Rodo Eventos";
 
 $enviado = mail_phpmailer_2018($destinatario_email, $destinatario_nombre, $remitente_email, $email, $nombre, $contenido_asunto, $cuerpo);
-
+echo $cuerpo;
+echo $enviado;
+echo ($enviado);
  if($enviado){
     echo 1;
  } else {
@@ -82,4 +84,3 @@ $enviado = mail_phpmailer_2018($destinatario_email, $destinatario_nombre, $remit
 
 ?>
  
-
