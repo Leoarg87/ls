09beec6d6c890b119dba8732275f7e9703380abe
Leoarg87 <leoarielgarcia87@gmail.com>
2023-02-04 12:29:02 +0100

    a

--- rodo/mail.php
+++ rodo/mail.php
@@ -4,9 +4,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ERROR);
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer/src/Exception.php';
-    require 'PHPMailer/src/PHPMailer.php';
-    require 'PHPMailer/src/SMTP.php';
+require '../PHPMailer/src/Exception.php';
+    require '../PHPMailer/src/PHPMailer.php';
+    require '../PHPMailer/src/SMTP.php';
   $error = "";
 
   function mail_phpmailer_2018($destinatario_email, $destinatario_nombre, $remitente_email, $nombre, $contenido_asunto, $contenido_html){
