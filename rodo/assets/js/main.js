1e9ec0bb04caab9bef77da39b79b426637f4bdaf
Leoarg87 <leoarielgarcia87@gmail.com>
2023-02-04 12:41:50 +0100

    a

--- rodo/assets/js/main.js
+++ rodo/assets/js/main.js
@@ -193,7 +193,7 @@ $('.opiniones').slick({
       console.log("js")
       $.ajax({
         type: "POST",
-        url: "../mandar_mail.php",
+        url: "mandar_mail.php",
         data: $("#form_contacto").serialize(),
         success: function (data) {
           
