a17cc845c2493b8aa156239d8d3844b631671b89
Leoarg87 <leoarielgarcia87@gmail.com>
2023-02-20 21:25:33 +0100

    a

--- rodo/inc/mandar_mail.php
+++ rodo/inc/mandar_mail.php
@@ -13,9 +13,9 @@
 
  function mail_phpmailer_2018($destinatario_email, $destinatario_nombre, $remitente_email, $nombre, $contenido_asunto, $contenido_html){
      global $error;
-     $paso ='LiceoRC4.';
+     $paso ='Rodo2311.';
      $host_smtp         = "smtp.hostinger.com";
-     $login_smtp        = "hola@leoscandura.es";
+     $login_smtp        = "hola@rodoevento.com";
      $password_smtp     = $paso;
      $nombre   = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["nombre"])));
      $email    = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
@@ -32,7 +32,7 @@
      $mail->Port       = 465;                    
      $mail->Host       = $host_smtp;  
 
-     $mail->SetFrom("hola@leoscandura.es", $nombre);
+     $mail->SetFrom("hola@rodoeventos.com", $nombre);
      $mail->AddAddress($destinatario_email, $destinatario_nombre);
      $mail->Subject    = $contenido_asunto;
      $mail->MsgHTML($contenido_html);
@@ -69,7 +69,7 @@
 
 
  $contenido_asunto =  "". $_POST["nombre"] . " Solicita informacion desde la web";
- $destinatario_email     =    "hola@leoscandura.es";
+ $destinatario_email     =    "hola@rodoeventos.com";
  $destinatario_nombre     =    "Rodo Eventos";
 $remitente_email = $_POST["email"];
 $nombre="Rodo";
